ef6485b113ad4a4b69daa0edce22e4d56aa1e31a
jthomas <jeremy.dh.thomas@gmail.com>
2024-04-28 22:15:40 -0700

    Card multiples grouped

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"mtgStack/identify"
 )
@@ -14,14 +15,14 @@ func main() {
 		log.Fatalf("Unable to read dir entries: %v", err)
 	}
 
-	var cards []string
+	cards := make(map[string]int)
 
 	for _, e := range entries {
 		cardName, err := identify.CardTitle("img/" + e.Name())
 		if err != nil {
 			fmt.Printf("main: unable to identify card title for img %s: %v", e.Name(), err)
 		}
-		cards = append(cards, cardName)
+		cards[cardName] = cards[cardName] + 1
 	}
 
 	output, err := os.Create("output.txt")
@@ -29,8 +30,8 @@ func main() {
 		log.Fatalf("main: unable to create output file: %v", err)
 	}
 
-	for _, card := range cards {
-		_, err = output.WriteString(card + "\n")
+	for card, count := range cards {
+		_, err = output.WriteString(strconv.Itoa(count) + " " + card + "\n")
 		if err != nil {
 			log.Printf("main: unable to write card %s to output file: %v", card, err)
 		}
